3fdeb45b15dfc903bfd4d938cff23c294147b994
petelkavictor <p.v.victor322@gmail.com>
2025-03-07 11:56:45 +0200

    init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/shop/:path*",
+        destination: `${process.env.SHOP_DOMAIN}/shop/:path*`, // Proxy to the Shop app
+      },
+    ];
+  },
 };
 
 export default nextConfig;
